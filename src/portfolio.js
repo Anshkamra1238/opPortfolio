577a0f1fb539029b276455cc89dcd0cd181146f5
Anshkamra1238 <anshkamra1238@gmail.com>
2024-12-07 12:26:48 +0530

    Code

--- src/portfolio.js
+++ src/portfolio.js
@@ -3,7 +3,7 @@ import Type from "./Type";
 // Website related settings
 const settings = {
   isSplash: true, // Change this to true if you want to use the splash screen.
-  useCustomCursor: true, // Change this to false if you want the good'ol cursor
+  useCustomCursor: false, // Change this to false if you want the good'ol cursor
   googleTrackingID: "UA-174238252-2",
 };
 
